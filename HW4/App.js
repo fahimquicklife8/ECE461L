8f07741754ce441ece8a9f1fd687de57744d96ce
Fahim Imtiaz <90858661+fahimquicklife8@users.noreply.github.com>
2024-06-17 16:24:36 -0500

    Add files via upload

---
+++ HW4/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Projects from './components/Projects';
+import './App.css'; // Make sure to have some styles defined here
+
+function App() {
+  return (
+    <div className="App">
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
